870b5045aeb6d47f47ec88ba0daa046ff802e759
garciafl <garcia.floriian@gmail.com>
2014-05-14 19:12:02 +0200

    Création de la faq

---
+++ faq.php
@@ -0,0 +1,198 @@
+<?php require 'header.html'; ?>
+<div class="container">
+  <div class="row row-right">
+    <div class="col-xs-12 col-sm-9">
+      <h1>Foire Aux Questions</h1>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Un apprenti doit-il payer les frais de dossier liés à son dépôt de candidature à l’UTC ?</h3>
+        </div>
+        <div class="panel-body">
+          Oui car avant de postuler à l’apprentissage le candidat postule d’abord à l’UTC. Son dossier est donc étudié par le jury d’admission. C’est dans un second temps que la section apprentissage donne un avis.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Un apprenti doit-il payer les frais de scolarité ?</h3>
+        </div>
+        <div class="panel-body">
+          Non, les apprentis ne payent pas de frais de scolarité, ils sont pris en charge par son entreprise.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Un candidat étranger peut-il postuler à l’apprentissage ?</h3>
+        </div>
+        <div class="panel-body">
+          Oui, des démarches sont à faire auprès de la préfecture pour obtenir une autorisation de travail sur sa carte de séjour.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Un candidat peut-il signer un contrat d’apprentissage avec une entreprise étrangère ?</h3>
+        </div>
+        <div class="panel-body">
+          Non, car le contrat d’apprentissage est un contrat de travail régi par le droit français.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Un stage à l’étranger est-il possible dans le cadre de l’apprentissage ?</h3>
+        </div>
+        <div class="panel-body">
+          Oui, il est même obligatoire, d’une durée d’au moins 12 semaines.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Un semestre de cours à l’étranger est-il possible dans le cadre de l’apprentissage ?</h3>
+        </div>
+        <div class="panel-body">
+          Non
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Un étudiant peut-il poursuivre la formation initiale standard si il ne trouve pas de contrat d’apprentissage ?</h3>
+        </div>
+        <div class="panel-body">
+          Oui, comme l’apprenti est d’abord accepté par l’UTC avant d’entrer en apprentissage, il peut retourner en filière classique s’il ne trouve pas d’entreprise capable de l’accueillir.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Le contrat d’apprentissage est-il obligatoirement de 3 ans ?</h3>
+        </div>
+        <div class="panel-body">
+          Non, les étudiants peuvent entrer en apprentissage après une année de cycle d’ingénieur en formation classique. C’est mesure est dérogatoire et nécessite qu’il reste encore des places dans la promotion.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Le contrat d’apprentissage est-il obligatoire sur toute sa durée dans la même entreprise ?</h3>
+        </div>
+        <div class="panel-body">
+          Oui, sauf cas exceptionnels (fermeture de l’entreprise, mauvaise entente entreprise - étudiant).
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Quel est l’âge limite pour postuler en apprentissage ?</h3>
+        </div>
+        <div class="panel-body">
+          Il faut avoir entre 16 et 26 ans pour postuler en apprentissage. Au-delà des dérogations sont possibles sous certaines conditions (création de sa propre entreprise par l’apprenti à la suite de l’obtention de son diplôme).
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Quels sont les diplômes recevables pour la formation d’ingénieur par apprentissage ?</h3>
+        </div>
+        <div class="panel-body">
+          Un étudiant qui possède un DUT ou une licence peut entrer dans la formation d’ingénieur par apprentissage. Les étudiants sortant de tronc commun peuvent bien entendu aussi rejoindre cette formation.
+          La liste des diplômes recevables est la même que pour la formation d’ingénieur standard selon le département où candidate l’apprenti (voir tableau en pj)
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Quel est le rythme de l’alternance ?</h3>
+        </div>
+        <div class="panel-body">
+          La première année, l’apprenti est présent pendant les vacances scolaires des étudiants en filière classique.<br />
+          La deuxième année, le rythme d’alternance est de 1 mois. L’apprenti est un mois en entreprise, un mois à l’école.<br />
+          Sur la première partie de la dernière année, l’apprenti garde ce rythme d’alternance d’un mois. Sur la seconde partie, l’apprenti est en entreprise la totalité du temps, soit pendant six mois.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">L’apprenti peut-il signer un contrat avec une entreprise hors de la région Picardie ?</h3>
+        </div>
+        <div class="panel-body">
+          Oui, bien sur.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Quelle est la distance autorisée entre le CFA et l’entreprise ?</h3>
+        </div>
+        <div class="panel-body">
+          Aucune, en dehors de la présence de l’entreprise sur le territoire français.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Comment gérer son double logement ?</h3>
+        </div>
+        <div class="panel-body">
+          Le meilleur moyen de gérer un double logement est de l’éviter. C’est à dire essayer de trouver une entreprise à proximité du lieu de villégiature des parents. Si ce n’est pas possible, il est aussi possible de trouver une entreprise à proximité de Compiègne, mais cela reste rare. Si le double logement est inévitable, il faut essayer d’optimiser l’APL afin que la somme reçue soit maximale. Ainsi, il faut simuler ses droits à l’APL sur le site de la CAF sur chacun des logements. Il faut savoir que les sommes perçues d’APL sont généralement plus élevées que pour un étudiant en filière classique. Une autre piste possible est l’aide MOBILI-JEUNE, qui permet d’alléger les quittances de loyer des apprentis.<br />
+          Plus d’informations : <a href="http://www.cilgere.fr/espace-salaries/mobilite/MOJ/">ici</a><br />
+          Certaines entreprises (souvent les grands groupes) ont aussi des possibilités de logements ou de financement de logement pour les petits salaires (donc les apprentis). Il faut donc vous renseigner auprès de votre entreprise.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Le diplôme obtenu par la voie de l’apprentissage est-il différent de celui obtenu par la voie classique ?</h3>
+        </div>
+        <div class="panel-body">
+          Non, c’est le même. La mention “par apprentissage” n’apparaît que sur le supplément au diplôme.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Peut-on encore percevoir une bourse si un contrat d’apprentissage est signé ?</h3>
+        </div>
+        <div class="panel-body">
+          Non, un apprenti n’est plus étudiant mais salarié. Ses droits du CROUS ne sont donc plus valables.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Peut-on faire un double diplôme par la voie de l’apprentissage ?</h3>
+        </div>
+        <div class="panel-body">
+          Non
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Peut-on suivre une filière par la voie de l’apprentissage ?</h3>
+        </div>
+        <div class="panel-body">
+          Non, mais il existe des parcours propres à l’apprentissage. Ils sont au nombre de deux en informatique et en mécanique.<br />
+          Pour la mécanique, les filières proposées sont “Conception” et “Industrialisation”.<br />
+          Pour l’informatique, les filières sont “Ingénierie logicielle” et “Ingénierie des systèmes d’information”.
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Quel est le salaire d’un apprenti ?</h3>
+        </div>
+        <div class="panel-body">
+          Le salaire d’un apprenti procède une borne inférieure qui varie selon l'âge et l’année d’apprentissage. Mais il s’agit uniquement d’une borne inférieure et de nombreux apprentis ont un salaire supérieur à cette borne.<br />
+          Cependant, il n’existe pas de règle. Voici la grille de salaire minimum :
+          <div style="text-align:center; margin-top: 10px;">
+            <img src="/Apprenteam/static/img/salaires.png" alt="salaire apprentis"/>
+              <br />Source : <a href="http://vosdroits.service-public.fr/particuliers/F10032.xhtml">Service public</a> (25 mars 2014)
+          </div>
+          <br />
+          Il faut savoir qu’il n’y a pas de différence entre le salaire brut et le salaire net d’un apprenti, les charges inexistantes en apprentissage.
+        </div>
+      </div>
+      <p class="main-content">
+        Pour tout autre question référez vous à la <a href="/Apprenteam/informations_administratives">section administrative</a> pour obtenir des adresses de contact.
+      </p>
+    </div>
+    <div class="col-md-3">
+      <div class="bs-docs-sidebar hidden-print" role="complementary">
+      <h3><span class="label label-default">Sites relatifs</span></h3>
+        <ul class="nav bs-docs-sidenav">
+          <li>
+          <li><a href="http://www.utc.fr/formations-enseignements/diplome-ingenieur-apprentissage.php" > Site de l'UTC Apprentissage</a></li>
+          <li><a href="http://www.irfa-apisup.fr/">Site du CFA IRFA API-SUP</a></li>
+          <li><a href="http://www.education.gouv.fr/cid155/se-former-par-l-apprentissage.html">Ministère de l'éducation</a></li>
+          <li><a href="http://www.utc.fr/" >Catalogue des UVs de l'UTC 2013/2014</a></li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+<?php require 'footer.html'; ?>
