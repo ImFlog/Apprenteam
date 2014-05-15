b25d1dc3489d047c105f358d63a5fcc357ef3933
garciafl <garcia.floriian@gmail.com>
2014-05-15 10:27:07 +0200

    Ajout JS FAQ

--- static/js/perso.js
+++ static/js/perso.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
   $('[data-toggle=offcanvas]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+    $('.row-offcanvas').toggleClass('active');
+  });
+
+  $('.expand').click(function() {
+    this.parentNode.parentNode.getElementsByClassName('panel-body')[0].setAttribute("class", "panel-body show");
+    this.setAttribute("class", "expand hidden");
+    this.parentNode.getElementsByClassName("hide")[0].setAttribute("class", "hide show");
+  });
+  $('.hide').click(function() {
+    this.parentNode.parentNode.getElementsByClassName('panel-body')[0].setAttribute("class", "panel-body hidden");
+    this.setAttribute("class", "hide hidden");
+    this.parentNode.getElementsByClassName("expand")[0].setAttribute("class", "expand show");
   });
 });
