e50943aea7dd9c88f9c26c9ed77464f29801a4fc
Brice <bonjgood@gmail.com>
2014-05-15 08:24:44 +0200

    Ajout des pages formations GI et GM + modif CSS

--- index.php
+++ index.php
@@ -1,48 +1,48 @@
 <?php require 'header.html'; ?>
 <div class="container">
-  <div class="row row-right">
-    <div class="col-xs-12 col-sm-9">
-      <p class="pull-right visible-xs">
-        <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
-      </p>
-      <div class="jumbotron">
-        <p>L'apprenteam est l'association des apprentis de l'<span class="info">UTC</span>.
-	   Elle a pour rôle de fédérer les apprentis et promouvoir l'apprentissage à l'UTC.</p>
-	<p style="text-align : right;"><a href="/Apprenteam/formation.php">Futurs apprentis, découvrez nos formations...</a></p>
-      </div>
-      <div class="row">
-        <div class="col-6 col-sm-6 col-lg-4">
-	        <img src="static/img/present/livre.png" class="img-rounded" />
-          <h2>Vidéos</h2>
-          <p>Les membres de l'administration, les enseignants/chercheurs responsablent de la formation ainsi que les actuels étudiants présentent vous présente les formations par apprentissage en vidéo .</p>
-          <p><a class="btn btn-default" href="#" role="button">Découvrez les vidéos</a></p>
-        </div>
-        <div class="col-6 col-sm-6 col-lg-4">
-		      <img src="static/img/present/livre.png" class="img-rounded" />
-          <h2>FAQ</h2>
-          <p>Découvrez toutes les réponses à toutes les questions que vous vous posez dans notre foire aux questions.</p>
-          <br /><br /><p><a class="btn btn-default" href="#" role="button">Réponses à vos questions</a></p>
-        </div>
-        <div class="col-6 col-sm-6 col-lg-4">
-          <img src="static/img/present/livre.png" class="img-rounded" />
-          <h2>Nous contacter</h2>
-          <p>Découvrez où nous <strong>rencontrer</strong>, salons, journées portes ouvertes et également comment nous <strong>contacter</strong></p>
-          <br /><br /><p><a class="btn btn-default" href="#" role="button">Nous contacter</a></p>
-        </div>
-      </div>
-    </div>
-  <div class="col-md-3">
-    <div class="bs-docs-sidebar hidden-print" role="complementary">
-	  <h3><span class="label label-default">Sites relatifs</span></h3>
-      <ul class="nav bs-docs-sidenav">
-        <li>
-				<li><a href="http://www.utc.fr/formations-enseignements/diplome-ingenieur-apprentissage.php" > Site de l'UTC Apprentissage</a></li>
-				<li><a href="http://www.irfa-apisup.fr/">Site du CFA IRFA API-SUP</a></li>
-				<li><a href="http://www.education.gouv.fr/cid155/se-former-par-l-apprentissage.html">Ministère de l'éducation</a></li>
-				<li><a href="http://www.utc.fr/" >Catalogue des UVs de l'UTC 2013/2014</a></li>
-		    </li>
-	    </ul>
-    </div>
-  </div>
-</div>
-<?php require 'footer.html'; ?>
+	<div class="row row-right">
+		<div class="col-xs-12 col-sm-9">
+			<p class="pull-right visible-xs">
+				<button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+			</p>
+			<div class="jumbotron">
+				<p>L'apprenteam est l'association des apprentis de l'<span class="info">UTC</span>.
+					Elle a pour rôle de fédérer les apprentis et promouvoir l'apprentissage à l'UTC.</p>
+					<p style="text-align : right;"><a href="/Apprenteam/formation.php">Futurs apprentis, découvrez nos formations...</a></p>
+				</div>
+				<div class="row">
+					<div class="col-6 col-sm-6 col-lg-4">
+					 <img src="static/img/present/livre.png" class="img-rounded" />
+					 <h2>Vidéos</h2>
+					 <p>Les membres de l'administration, les enseignants/chercheurs responsablent de la formation ainsi que les actuels étudiants présentent vous présente les formations par apprentissage en vidéo .</p>
+					 <p><a class="btn btn-default" href="#" role="button">Découvrez les vidéos</a></p>
+				 </div>
+				 <div class="col-6 col-sm-6 col-lg-4">
+					<img src="static/img/present/livre.png" class="img-rounded" />
+					<h2>FAQ</h2
+						<p>Découvrez toutes les réponses à toutes les questions que vous vous posez dans notre foire aux questions.</p>
+						<br /><br /><p><a class="btn btn-default" href="#" role="button">Réponses à vos questions</a></p>
+					</div>
+					<div class="col-6 col-sm-6 col-lg-4">
+						<img src="static/img/present/livre.png" class="img-rounded" />
+						<h2>Nous contacter</h2>
+						<p>Découvrez où nous <strong>rencontrer</strong>, salons, journées portes ouvertes et également comment nous <strong>contacter</strong></p>
+						<br /><br /><p><a class="btn btn-default" href="#" role="button">Nous contacter</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="bs-docs-sidebar hidden-print" role="complementary">
+				 <h3><span class="label label-default">Sites relatifs</span></h3>
+				 <ul class="nav bs-docs-sidenav">
+					<li>
+						<li><a href="http://www.utc.fr/formations-enseignements/diplome-ingenieur-apprentissage.php" > Site de l'UTC Apprentissage</a></li>
+						<li><a href="http://www.irfa-apisup.fr/">Site du CFA IRFA API-SUP</a></li>
+						<li><a href="http://www.education.gouv.fr/cid155/se-former-par-l-apprentissage.html">Ministère de l'éducation</a></li>
+						<li><a href="http://www.utc.fr/" >Catalogue des UVs de l'UTC 2013/2014</a></li>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<?php require 'footer.html'; ?>
